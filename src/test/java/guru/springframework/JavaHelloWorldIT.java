e1a7e8d35a18c71001331d011f4b3afe4946f29d
Phil Dolanov <phildolganov@yahoo.com>
2019-07-13 11:02:28 -0700

    added failsafe support

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("My IT Ran");
+    }
+}
